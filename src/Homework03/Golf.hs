4afbfca56621dfae569796295568b6f21784456f
Maylon Pedroso <maylonpedroso@gmail.com>
2019-05-22 22:22:34 -0500

    Homework03 solutions

--- src/Homework03/Golf.hs
+++ src/Homework03/Golf.hs
@@ -7,61 +7,98 @@
 
 module Homework03.Golf where
 
-----------------------------------------------------------------------
--- Exercise 1
-----------------------------------------------------------------------
+    import Data.List
+    import Data.Maybe
 
--- |
---
--- >>> skips "ABCD"
--- ["ABCD","BD","C","D"]
--- >>> skips "hello!"
--- ["hello!","el!","l!","l","o","!"]
--- >>> skips [1]
--- [[1]]
--- >>> skips [True, False]
--- [[True,False],[False]]
--- >>> skips []
--- []
+    ----------------------------------------------------------------------
+    -- Exercise 1
+    ----------------------------------------------------------------------
 
-skips :: [a] -> [[a]]
-skips = undefined
+    -- |
+    --
+    -- >>> skips "ABCD"
+    -- ["ABCD","BD","C","D"]
+    -- >>> skips "hello!"
+    -- ["hello!","el!","l!","l","o","!"]
+    -- >>> skips [1]
+    -- [[1]]
+    -- >>> skips [True, False]
+    -- [[True,False],[False]]
+    -- >>> skips []
+    -- []
 
-----------------------------------------------------------------------
--- Exercise 2
-----------------------------------------------------------------------
+    skip :: Int -> [a] -> [a]
+    skip n l
+        | n < length l = ((drop n l) !! 0) : skip n (drop (n+1) l)
+        | otherwise = []
 
--- |
---
--- >>> localMaxima [2,9,5,6,1]
--- [9,6]
--- >>> localMaxima [2,3,4,1,5]
--- [4]
--- >>> localMaxima [1,2,3,4,5]
--- []
+    skips :: [a] -> [[a]]
+    skips l = map f [0..(length l)-1] where f n = skip n l
 
-localMaxima :: [Integer] -> [Integer]
-localMaxima = undefined
+    ----------------------------------------------------------------------
+    -- Exercise 2
+    ----------------------------------------------------------------------
 
-----------------------------------------------------------------------
--- Exercise 3
-----------------------------------------------------------------------
+    -- |
+    --
+    -- >>> localMaxima [2,9,5,6,1]
+    -- [9,6]
+    -- >>> localMaxima [2,3,4,1,5]
+    -- [4]
+    -- >>> localMaxima [1,2,3,4,5]
+    -- []
 
--- |
---
--- >>> putStr (histogram [1,1,1,5])
---  *
---  *
---  *   *
--- ==========
--- 0123456789
--- >>> putStr (histogram [1,4,5,4,6,6,3,4,2,4,9])
---     *
---     *
---     * *
---  ******  *
--- ==========
--- 0123456789
+    localMaxima :: [Integer] -> [Integer]
+    localMaxima (a:b:c:l)
+        | a < b && b > c = b:(localMaxima (c:l))
+        | otherwise      = localMaxima (b:c:l)
+    localMaxima _ = []
 
-histogram :: [Integer] -> String
-histogram = undefined
+    ----------------------------------------------------------------------
+    -- Exercise 3
+    ----------------------------------------------------------------------
+
+    -- |
+    --
+    -- >>> putStr (histogram [1,1,1,5])
+    --  *
+    --  *
+    --  *   *
+    -- ==========
+    -- 0123456789
+    -- >>> putStr (histogram [1,4,5,4,6,6,3,4,2,4,9])
+    --     *
+    --     *
+    --     * *
+    --  ******  *
+    -- ==========
+    -- 0123456789
+
+    histogram :: [Integer] -> String
+    histogram l = generateCounters (counter l)
+
+    generateCounters :: [Integer] -> String
+    generateCounters l
+        | max_ == 0 = "==========\n0123456789\n"
+        | otherwise = generateRow indices ++ generateCounters next_
+        where max_ = maximum l
+              indices = elemIndices max_ l
+              getVal :: Int -> Integer
+              getVal x
+                  | elemIndex x indices == Nothing = l !! x
+                  | otherwise = (l !! x) - 1
+              next_ = [getVal i | i <- [0..9]]
+
+    generateRow :: [Int] -> String
+    generateRow n = [generatePixel n i | i <- [0..9]] ++ "\n"
+
+    generatePixel :: [Int] -> Int -> Char
+    generatePixel l x
+        | elemIndex x l == Nothing = ' '
+        | otherwise = '*'
+
+    counter :: [Integer] -> [Integer]
+    counter [] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    counter (x:l) = take n c ++ [(c !! n) + 1] ++ drop (n + 1) c
+        where c = counter(l)
+              n = fromIntegral x
