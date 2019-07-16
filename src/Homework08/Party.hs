abf1c2c1710f127c456a17318ffbfb545e882cb1
Maylon Pedroso <maylonpedroso@gmail.com>
2019-07-15 19:46:45 -0500

    Improve homework 8 solution

--- src/Homework08/Party.hs
+++ src/Homework08/Party.hs
@@ -8,7 +8,9 @@
 module Homework08.Party where
 
 import Homework08.Employee
+import Data.List
 import Data.Tree
+import Data.Semigroup
 
 ----------------------------------------------------------------------
 -- Exercise 1
@@ -17,46 +19,46 @@ import Data.Tree
 glCons :: Employee -> GuestList -> GuestList
 glCons employee (GL list fun) = GL (list++[employee]) (fun + (empFun employee)) 
 
+instance Semigroup GuestList where
+    (GL l1 f1) <> (GL l2 f2) = GL (l1 ++ l2) (f1 + f2)
+
 instance Monoid GuestList where
     mempty = GL [] 0
-    (GL l1 f1) `mappend` (GL l2 f2) = GL (l1 ++ l2) (f1 + f2) 
+    mappend = (<>) 
 
-moreFun :: GuestList -> GuestList -> GuestList
-moreFun l1 l2 = if l1 > l2 then l1 else l2
+moreFun :: (GuestList, GuestList) -> GuestList
+moreFun (l1, l2) = if l1 > l2 then l1 else l2
 
 
 ----------------------------------------------------------------------
 -- Exercise 2
 ----------------------------------------------------------------------
 
-treeFold :: (a -> [Tree a] -> b) -> Tree a -> b
-treeFold func (Node value list) = func value list
+treeFold :: (a -> [b] -> b) -> Tree a -> b
+treeFold func (Node value list) = func value (map (treeFold func) list)
 
 ----------------------------------------------------------------------
 -- Exercise 3
 ----------------------------------------------------------------------
 
 nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
-nextLevel bob list = (glCons bob (mconcat (map snd list)), mconcat (map fst list))
+nextLevel bob list = (glCons bob (mconcat (map snd list)), mconcat (map moreFun list))
 
 
 ----------------------------------------------------------------------
 -- Exercise 4
 ----------------------------------------------------------------------
 
-maxFunTuple :: (GuestList, GuestList) -> GuestList
-maxFunTuple (a, b) = moreFun a b
-
-processNode :: Tree Employee -> (GuestList, GuestList)
-processNode (Node employee list) = nextLevel employee $ map processNode list
-
 maxFun :: Tree Employee -> GuestList
-maxFun tree = maxFunTuple $ processNode tree
-
+maxFun tree = moreFun $ treeFold nextLevel tree
 
 ----------------------------------------------------------------------
 -- Exercise 5
 ----------------------------------------------------------------------
 
 main :: IO ()
-main = undefined
+main = do
+    content <- readFile "Homework08/company.txt"
+    let GL result fun = maxFun (read content :: Tree Employee)
+    putStr $ "Total Fun: " ++ (show fun) ++ "\n"
+    putStr $ unlines $ sort $ map empName result
